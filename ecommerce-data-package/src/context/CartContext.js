b5f7bdbacd3ba82c74a821f577708dcf4b7db33c
mochamad afif solahudin <mafifsolahudin@gmail.com>
2025-06-26 18:01:17 +0700

    Add ecommerce-data-package/src/context/CartContext.js 1750935677002

---
+++ ecommerce-data-package/src/context/CartContext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function useCart() {
+  return useContext(CartContext);
+}
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCart(cart.filter((item) => item.id !== itemId));
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const getCartTotal = () => {
+    return cart.reduce((total, item) => total + item.price, 0);
+  };
+
+  const value = {
+    cart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    getCartTotal,
+  };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+}
